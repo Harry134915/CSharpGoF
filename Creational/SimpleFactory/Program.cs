d61f9d3dba6c5209328986eb44c79bcc8e3eacb9
Jacky Jiang <765643729@163.com>
2026-03-08 14:51:42 +0800

    初始化项目

---
+++ Creational/SimpleFactory/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleFactory;
+
+internal class Program
+{
+    /// <summary>
+    /// 游戏里有三种武器： Sword（剑）, Bow（弓）, Staff（法杖）, 你需要创建他们的实例来使用他们.
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        //未使用简单工厂模式
+        
+        
+
+        //使用简单工厂模式
+
+        Console.WriteLine("Hello, SimpleFactory!");
+    }
+}
